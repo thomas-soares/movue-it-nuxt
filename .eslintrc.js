182b6a86fbd192288f9d27928e3f750205c32216
Thomas Soares <thomas__soares@hotmail.com>
2023-04-28 00:55:42 -0300

    configurando projeto

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    'jest/globals': true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'jest'
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		'jest/globals': true,
+		node: true,
+	},
+	extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'prettier'],
+	plugins: ['prettier', 'jest'],
+	// add your custom rules here
+	rules: {
+		// 'prettier/prettier': ['error'],
+		// 'comma-dangle': ['error', 'always-multiline'],
+		// 'import/first': 'off',
+		// 'import/order': 'off',
+		// indent: 'off',
+		// '@typescript-eslint/indent': ['error', 'tab'],
+		// 'no-new': 'off',
+		// 'no-tabs': 'off',
+		// semi: ['error', 'always'],
+		// 'vue/html-indent': ['error', 'tab'],
+		// 'vue/order-in-components': 'off',
+	},
 }
